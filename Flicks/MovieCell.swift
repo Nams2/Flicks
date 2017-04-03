122fa2afb536071d8dbea8d3ad29407871a40edd
Namrata Mehta <namrata@Gauravs-MBP.hsd1.ca.comcast.net>
2017-04-02 17:57:40 -0700

    first commit

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Namrata Mehta on 3/31/17.
+//  Copyright © 2017 Namrata Mehta. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flicks/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flicks
+//
+//  Created by Namrata Mehta on 4/2/17.
+//  Copyright © 2017 Namrata Mehta. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
